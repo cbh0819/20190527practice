e95a2dd7cbe8e1fe909c627a14ed4ff296bbd958
Andy0414 <pjh8667@naver.com>
2019-05-28 01:11:07 +0900

    UPDATE die

--- testfile/js/playManager.js
+++ testfile/js/playManager.js
@@ -1,6 +1,8 @@
-class PlayManager{
-    constructor(){
-        this.bet = 10;
+class PlayManager {
+    constructor() {
+        this.bet = 0
+        this.isStart = false
+        this.round = 1
 
         this.deck = new Deck()
 
@@ -10,16 +12,43 @@ class PlayManager{
 
         this.mid = document.getElementById("mid")
     }
-    start(){
-        this.mid.innerText = this.bet
-
-        this.players.forEach(x=>{
+    start() {
+        this.isStart = true
+        this.betting(10)
+        this.players.forEach(x => {
             x.addCards(this.deck.draw(3))
         })
-    }
-    betting() {
-        if (this.owner.spendMoeny(Math.floor(this.bet / 2)))
-            this.bet += Math.floor(this.bet/2)
         this.mid.innerText = this.bet
+    }
+    betting(num) {
+        if (this.isStart) {
+            var value = num || Math.floor(this.bet / 2)
+            this.players.forEach(x => {
+                if (x.isDie) return
+                if (x.spendMoeny(value)) {
+                    this.bet += value
+                } else
+                    x.die()
+            })
+            this.mid.innerText = this.bet
+        }
+    }
+    go() {
+        if (this.isStart && this.round < 3) {
+            this.round++
+            this.betting()
+            this.deal()
+        }
+    }
+    deal() {
+        if (this.isStart) {
+            this.players.forEach(x => {
+                x.addCard(this.deck.draw())
+            })
+        }
+    }
+    die() {
+        if (this.isStart)
+            this.owner.die()
     }
 }
